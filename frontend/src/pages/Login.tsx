82d71d1978786ef9164148824df86279ae292445
KhaanFaizan <faizan.learner@gmail.com>
2025-06-26 14:58:14 +0500

    View Projects added

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -8,7 +8,6 @@ const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
